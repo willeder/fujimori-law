06deff40f0b5ce57708895b4b3ad4eb4e9751047
Willeder <86581664+willeder@users.noreply.github.com>
2026-03-31 02:08:45 +0900

    fix router error

--- client-mock/src/App.tsx
+++ client-mock/src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from 'react-router-dom'
-import { router } from './app/router'
+//import { router } from './app/router'
 
 export default function App() {
   return <RouterProvider router={router} />
